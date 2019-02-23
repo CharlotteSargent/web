d29c7c2651a8d836737141b66e738881d8de6d86
Charlotte Sargent <47238711+CharlotteSargent@users.noreply.github.com>
2019-02-23 10:38:35 +1000

    Add files via upload

---
+++ project_1/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+	return HttpResponse(request, "<h1>Home</h1>")
+
+def about(request):
+	return HttpResponse(request, "<h1>About</h1>")
